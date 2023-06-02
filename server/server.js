d0b606e874dbc9cb57aaf6dd51d08a8d3d852b76
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-06-02 15:40:23 -0500

    express server listens on port defined in .env

---
+++ server/server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+require("dotenv").config();
+
+const PORT = process.env.PORT;
+const app = express();
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
