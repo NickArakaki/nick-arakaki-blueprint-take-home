fbeb221d397456ef5cd185d9ee1971296e9b2e34
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-06-02 16:52:12 -0500

    add error formatter

--- server/routes/api/survey.js
+++ server/routes/api/survey.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 const survey = require("../../db/survey");
+const { validateSurvey } = require("../../utils/surveys/middleware");
 
 router.get("/", (_req, res) => {
   res.json(survey);
 });
 
-router.post("/", (req, res) => {
+router.post("/", validateSurvey, (req, res) => {
   res.json({ message: "hello from survery post route" });
 });
 
--- server/server.js
+++ server/server.js
@@ -8,4 +8,15 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
+// Error formatter
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    message: err.message,
+    statusCode: err.status || 500,
+    errors: err.errors,
+  });
+});
+
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
