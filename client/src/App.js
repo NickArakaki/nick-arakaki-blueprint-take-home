a5c1e1943fea0da7b2c87d2f985881734784ac93
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-06-06 16:28:02 -0500

    replace diagnostic screener component with diagnostic screener page

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import DiagnosticScreener from "./components/DiagnosticScreener";
+import Home from "./pages/Home/Home";
+import DiagnosticScreener from "./pages/DiagnosticScreener/DiagnosticScreener";
 import NotFound from "./pages/NotFound";
 
 function App() {
