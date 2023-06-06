058171cbaf1197a385bd5e16853bcc8d0d485553
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-06-06 16:31:19 -0500

    serve React index.html from non /api routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,28 @@
+const express = require("express");
+const router = express.Router();
 const apiRouter = require("./api");
-const router = require("express").Router();
 
 router.use("/api", apiRouter);
+
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+
+  // Serve the client's index.html file at the root route
+  router.get("/", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "../../client", "build", "index.html")
+    );
+  });
+
+  // Serve the static assets in the client's build folder
+  router.use(express.static(path.resolve("../client/build")));
+
+  // Serve the frontend's index.html file at all other routes NOT starting with /api
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "../../client", "build", "index.html")
+    );
+  });
+}
 
 module.exports = router;
